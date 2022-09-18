8cc9e5ca44b631b82d238b41a13596003ba1c5fa
Dhruv Chhotani <87544489+dhruv5712@users.noreply.github.com>
2022-09-18 18:06:19 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+  $('.sidenav').sidenav();
+  $('.materialboxed').materialbox();
+  $('.scrollspy').scrollSpy();
+  $('.parallax').parallax();
+
+  /* ScrollReveal allows the selected class to be revealed as the page is scrolled down*/
+  ScrollReveal().reveal('.scrollReveal');
+});
+ 
+/*
+let firstIntroMessage = document.getElementById("first-fade");
+firstIntroMessage.style.visibility = "hidden";
+let secondIntroMessage = document.getElementById("second-fade")
+secondIntroMessage.style.visibility = "hidden";
+let thirdIntroMessage = document.getElementById("third-fade")
+thirdIntroMessage.style.visibility = "hidden";
+let fourthIntroMessage = document.getElementById("fourth-fade")
+fourthIntroMessage.style.visibility = "hidden";
+let header = document.getElementById("header");
+window.onload = (event) => {
+    firstIntroMessage.style.visibility = "visible";
+    secondIntroMessage.style.visibility = "visible";
+    thirdIntroMessage.style.visibility = "visible";
+    fourthIntroMessage.style.visibility = "visible";
+};
+*/
